cacbc1e68547bf9f300ebae8666d2d497765ca98
David Fraley <fraleyds@gmail.com>
2017-11-14 16:42:19 -0500

    messed things up

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -3,10 +3,12 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace ElevenNote.Data
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    // You can add profile data for the user by adding more properties to your ElevenNoteUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ElevenNoteUser : IdentityUser
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ElevenNoteUser> manager)
@@ -18,16 +20,42 @@ namespace ElevenNote.Data
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ElevenNoteUser>
+    public class ElevenNoteDbContext : IdentityDbContext<ElevenNoteUser>
     {
-        public ApplicationDbContext()
+        public ElevenNoteDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
-        public static ApplicationDbContext Create()
+        public static ElevenNoteDbContext Create()
         {
-            return new ApplicationDbContext();
+            return new ElevenNoteDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                    .Add(new IdentityUserLoginConfiguration())
+                    .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.RoleId);
         }
     }
 }
